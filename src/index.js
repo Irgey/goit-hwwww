71b9242911936756acc053583c8ce0fd444693c6
Irgey <116591589+Irgey@users.noreply.github.com>
2023-03-27 15:11:26 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -14,42 +14,54 @@ inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 function onInputChange(e) {
   const inputValue = e.target.value.trim();
   if (inputValue !== '') {
-    fetchCountries(inputValue).then(result => {
-      if (result.length > 10) {
-        listEl.innerHTML = '';
+    fetchCountries(inputValue)
+      .then(result => {
+        if (result.length > 10) {
+          listEl.innerHTML = '';
 
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else if (result.length >= 2 && result.length <= 10) {
-        divEl.innerHTML = '';
-        const listItems = result.reduce(
-          (acc, { flags: { svg: flag }, name: { common: countryName } }) =>
-            acc +
-            `<li class = "list__item"><img class='list__img' src="${flag}" alt="Flag of ${countryName}">  ${countryName}</li>`,
-          ''
-        );
-        listEl.innerHTML = listItems;
-      } else if (result.length === 1) {
-        console.log(result);
-        listEl.innerHTML = '';
-        const {
-          flags: { svg: flag },
-          name: { common: countryName },
-          capital,
-          population,
-          languages,
-        } = result[0];
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (result.length >= 2 && result.length <= 10) {
+          console.log(result);
+          divEl.innerHTML = '';
+          const listItems = result.reduce(
+            (acc, { flags: { svg: flag }, name: { common: countryName } }) =>
+              acc +
+              `<li class = "list__item"><img class='list__img' src="${flag}" alt="Flag of ${countryName}">  ${countryName}</li>`,
+            ''
+          );
+          listEl.innerHTML = listItems;
+        } else if (result.length === 1) {
+          console.log(result);
+          listEl.innerHTML = '';
+          const {
+            flags: { svg: flag },
+            name: { common: countryName },
+            capital,
+            population,
+            languages,
+          } = result[0];
 
-        const countryCard = `<h2><img class="card__img" src="${flag}" alt="Flag of ${countryName}" />${countryName}</h2>
+          const countryCard = `<h2><img class="card__img" src="${flag}" alt="Flag of ${countryName}" />${countryName}</h2>
 <ul>
   <li><b>Capital:</b> ${capital.join(', ')}</li>
   <li><b>Population:</b> ${population}</li>
   <li><b>Languages:</b> ${Object.values(languages).join(', ')}</li>
 </ul>`;
-        divEl.innerHTML = countryCard;
-      }
-    });
+          divEl.innerHTML = countryCard;
+        }
+      })
+      .catch(error => {
+        console.dir(error);
+        if (error.message === '404') {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        } else {
+          Notiflix.Notify.failure(
+            'Something went wrong.Try again in few minutes'
+          );
+        }
+      });
   } else {
     divEl.innerHTML = '';
     listEl.innerHTML = '';
